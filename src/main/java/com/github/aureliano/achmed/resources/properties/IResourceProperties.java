cb70fcd41295ffa5784498cfb15dd8878cd23e83
aureliano <aureliano.franca@hotmail.com>
2015-12-01 18:13:17 -0200

    Create resource properties interface.

---
+++ src/main/java/com/github/aureliano/achmed/resources/properties/IResourceProperties.java
@@ -0,0 +1,8 @@
+package com.github.aureliano.achmed.resources.properties;
+
+public interface IResourceProperties {
+
+	public abstract IResourceProperties put(String key, Object value);
+	
+	public abstract Object get(String key);
+}
