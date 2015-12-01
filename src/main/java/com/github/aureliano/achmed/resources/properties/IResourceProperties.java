4f037a15329c960fc769172393ada47ad73bf339
aureliano <aureliano.franca@hotmail.com>
2015-12-01 19:09:57 -0200

    Add abstract method for parsing attributes.

--- src/main/java/com/github/aureliano/achmed/resources/properties/IResourceProperties.java
+++ src/main/java/com/github/aureliano/achmed/resources/properties/IResourceProperties.java
@@ -5,4 +5,6 @@ public interface IResourceProperties {
 	public abstract IResourceProperties put(String key, Object value);
 	
 	public abstract Object get(String key);
+	
+	public abstract void configureAttributes();
 }
