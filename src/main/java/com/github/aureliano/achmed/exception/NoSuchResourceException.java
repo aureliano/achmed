d5d237bec7841c506158763e30fba2fbb5fa13de
aureliano <aureliano.franca@hotmail.com>
2015-12-02 17:06:46 -0200

    Create exception to unsupported or unknown resource types.

---
+++ src/main/java/com/github/aureliano/achmed/exception/NoSuchResourceException.java
@@ -0,0 +1,22 @@
+package com.github.aureliano.achmed.exception;
+
+public class NoSuchResourceException extends RuntimeException {
+
+	private static final long serialVersionUID = 140440539843566483L;
+
+	public NoSuchResourceException() {
+		super();
+	}
+
+	public NoSuchResourceException(String message) {
+		super(message);
+	}
+
+	public NoSuchResourceException(Throwable throwable) {
+		super(throwable);
+	}
+
+	public NoSuchResourceException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+}
