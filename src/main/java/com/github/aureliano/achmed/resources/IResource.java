4444ca1e4d4e783859a6608fbee918dab25bfad5
aureliano <aureliano.franca@hotmail.com>
2015-12-02 17:22:59 -0200

    Change method name from make to apply.

--- src/main/java/com/github/aureliano/achmed/resources/IResource.java
+++ src/main/java/com/github/aureliano/achmed/resources/IResource.java
@@ -4,7 +4,7 @@ import com.github.aureliano.achmed.resources.properties.IResourceProperties;
 
 public interface IResource {
 
-	public abstract void make(IResourceProperties properties);
+	public abstract void apply(IResourceProperties properties);
 	
 	public abstract ResourceType type();
 }
