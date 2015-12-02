37a2b9d2ccc37504d732ef7be2d5567a0b5951d5
aureliano <aureliano.franca@hotmail.com>
2015-12-02 13:48:59 -0200

    Add attribute ancestor to operating system type.

--- src/main/java/com/github/aureliano/achmed/resources/types/OperatingSystemFamily.java
+++ src/main/java/com/github/aureliano/achmed/resources/types/OperatingSystemFamily.java
@@ -2,8 +2,18 @@ package com.github.aureliano.achmed.resources.types;
 
 public enum OperatingSystemFamily {
 
-	DEBIAN,
-	UBUNTU,
-	RPM,
-	APP
+	DEBIAN(null),
+	UBUNTU(DEBIAN),
+	RPM(null),
+	APP(null);
+	
+	private OperatingSystemFamily ancestor;
+	
+	private OperatingSystemFamily(OperatingSystemFamily ancestor) {
+		this.ancestor = ancestor;
+	}
+	
+	public OperatingSystemFamily getAncestor() {
+		return ancestor;
+	}
 }
