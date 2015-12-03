74d494ee637c39d1c650f6ae44af9de26220c05d
aureliano <aureliano.franca@hotmail.com>
2015-12-03 11:23:36 -0200

    Create command runner.

---
+++ src/main/java/com/github/aureliano/achmed/resources/exec/CommandRunner.java
@@ -0,0 +1,66 @@
+package com.github.aureliano.achmed.resources.exec;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import com.github.aureliano.achmed.exception.ExecResourceException;
+
+public class CommandRunner implements Runnable {
+
+	private List<String> command;
+	private File workingDir;
+	private boolean verbose;
+	
+	public CommandRunner(String command) {
+		this.command = Arrays.asList(command.split("\\s+"));
+	}
+	
+	public void run() {
+		this.execute();
+	}
+	
+	public int execute() {
+		ProcessBuilder builder = new ProcessBuilder(this.command);
+		builder.directory(this.workingDir);
+		
+		int exitStatus = Integer.parseInt("1" + this.command.size());
+		Process process = null;
+		
+		try {
+			process = builder.start();
+			this.scanCommand(process);
+			exitStatus = process.waitFor();
+		} catch (Exception ex) {
+			try {
+				this.readError(process);
+			} catch (RuntimeException re) {
+				throw new ExecResourceException(re);
+			}
+		}
+		
+		return exitStatus;
+	}
+	
+	private void scanCommand(Process process) {
+		this.consumeStream(process.getInputStream(), this.verbose);
+	}
+	
+	private void readError(Process process) {
+		this.consumeStream(process.getErrorStream(), true);
+	}
+	
+	private void consumeStream(InputStream stream, boolean verbose) {
+		Scanner scanner = new Scanner(stream);
+		
+		while (scanner.hasNextLine()) {
+			if (verbose) {
+				System.out.println(scanner.nextLine());
+			}
+		}
+		
+		scanner.close();
+	}
+}
