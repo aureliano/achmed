a516c3a6298dcc879c15a4a922a9413f7b47294d
aureliano <aureliano.franca@hotmail.com>
2015-12-03 17:08:17 -0200

    Create operating system factory.

---
+++ src/main/java/com/github/aureliano/achmed/os/OperatingSystemFactory.java
@@ -0,0 +1,29 @@
+package com.github.aureliano.achmed.os;
+
+import com.github.aureliano.achmed.command.CommandFacade;
+import com.github.aureliano.achmed.exception.UnsupportedOperatingSystemException;
+import com.github.aureliano.achmed.helper.StringHelper;
+
+public final class OperatingSystemFactory {
+
+	private OperatingSystemFactory() {
+		throw new InstantiationError(this.getClass().getName() + " cannot be instantiated.");
+	}
+	
+	public static IOperatingSystem currentOperatingSystem() {
+		String os = System.getProperty("os.name");
+		
+		if ("Linux".equalsIgnoreCase(os)) {
+			return resolveLinux();
+		} else {
+			throw new UnsupportedOperatingSystemException("There is no support to the Operating System '" + os + "'.");
+		}
+	}
+	
+	protected static IOperatingSystem resolveLinux() {
+		String linux = CommandFacade.executeCommand("cat /etc/issue").getOutput();
+		linux = StringHelper.match("^\\w+", linux);
+		
+		return LinuxResolver.resolveLinux(linux);
+	}
+}
