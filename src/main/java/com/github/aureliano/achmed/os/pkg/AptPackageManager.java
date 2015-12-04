3849272c1c4c2fbc59dea61d1a5a2fc3b78378d6
aureliano <aureliano.franca@hotmail.com>
2015-12-04 14:17:08 -0200

    Implementation of "latest" method to Apt package manager.

--- src/main/java/com/github/aureliano/achmed/os/pkg/AptPackageManager.java
+++ src/main/java/com/github/aureliano/achmed/os/pkg/AptPackageManager.java
@@ -1,10 +1,20 @@
 package com.github.aureliano.achmed.os.pkg;
 
+import java.io.File;
+
+import com.github.aureliano.achmed.command.CommandBuilder;
+import com.github.aureliano.achmed.command.CommandFacade;
 import com.github.aureliano.achmed.command.CommandResponse;
+import com.github.aureliano.achmed.exception.PackageResourceException;
+import com.github.aureliano.achmed.helper.StringHelper;
+import com.github.aureliano.achmed.idiom.LanguageSingleton;
 import com.github.aureliano.achmed.resources.properties.PackageProperties;
 
 public class AptPackageManager implements IPackageManager {
 
+	private static final String APT_GET = "/usr/bin/apt-get";
+	private static final String APT_CACHE = "/usr/bin/apt-cache";
+	
 	private PackageProperties properties;
 	
 	public AptPackageManager() {
@@ -20,7 +30,17 @@ public class AptPackageManager implements IPackageManager {
 	}
 
 	public String latest() {
-		throw new UnsupportedOperationException("Method not implemented yet.");
+		String cmd = String.format("%s policy %s", APT_CACHE, this.properties.getName());
+		CommandResponse res = CommandFacade.executeCommand(this.buildCommand(cmd));
+		
+		if (!res.isOK()) {
+			throw new PackageResourceException(res.getError());
+		}
+		
+		String candidate = LanguageSingleton.instance().getValue("resource.package.apt.cache.latest_candidate_version");
+		String version = StringHelper.match(candidate + ":\\s+(\\S+)\\s", res.getOutput())[1];
+		
+		return version;
 	}
 
 	public void setPackageProperties(PackageProperties properties) {
@@ -29,5 +49,14 @@ public class AptPackageManager implements IPackageManager {
 
 	public PackageProperties getPackageProperties() {
 		return this.properties;
+	}
+	
+	private CommandBuilder buildCommand(String cmd) {
+		return new CommandBuilder()
+			.withCommand(cmd)
+			.withVerbose(false)
+			.withWorkingDir(new File("").getAbsolutePath())
+			.withTries(1)
+			.withTimeout(10000L);
 	}
 }
