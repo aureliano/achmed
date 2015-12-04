9a2119fd543e9f40319f8c656fcf91276910b8e4
aureliano <aureliano.franca@hotmail.com>
2015-12-04 10:31:49 -0200

    Create unsupported package manager exception.

---
+++ src/main/java/com/github/aureliano/achmed/exception/UnsupportedPackageManagerException.java
@@ -0,0 +1,22 @@
+package com.github.aureliano.achmed.exception;
+
+public class UnsupportedPackageManagerException extends RuntimeException {
+
+	private static final long serialVersionUID = -4089891118124103597L;
+
+	public UnsupportedPackageManagerException() {
+		super();
+	}
+
+	public UnsupportedPackageManagerException(String message) {
+		super(message);
+	}
+
+	public UnsupportedPackageManagerException(Throwable throwable) {
+		super(throwable);
+	}
+
+	public UnsupportedPackageManagerException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+}
