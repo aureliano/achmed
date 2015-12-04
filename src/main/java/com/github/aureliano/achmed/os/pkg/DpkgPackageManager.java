ce0f9371671db3e5fb4d31d93bcab9ddf8ea6e91
aureliano <aureliano.franca@hotmail.com>
2015-12-04 10:33:17 -0200

    Create template of dpkg package manager.

---
+++ src/main/java/com/github/aureliano/achmed/os/pkg/DpkgPackageManager.java
@@ -0,0 +1,33 @@
+package com.github.aureliano.achmed.os.pkg;
+
+import com.github.aureliano.achmed.command.CommandResponse;
+import com.github.aureliano.achmed.resources.properties.PackageProperties;
+
+public class DpkgPackageManager {
+
+	private PackageProperties properties;
+	
+	public DpkgPackageManager() {
+		super();
+	}
+
+	public CommandResponse install() {
+		throw new UnsupportedOperationException("Method not implemented yet.");
+	}
+
+	public CommandResponse uninstall() {
+		throw new UnsupportedOperationException("Method not implemented yet.");
+	}
+
+	public String latest() {
+		throw new UnsupportedOperationException("Method not implemented yet.");
+	}
+
+	public void setPackageProperties(PackageProperties properties) {
+		this.properties = properties;
+	}
+
+	public PackageProperties getPackageProperties() {
+		return this.properties;
+	}
+}
