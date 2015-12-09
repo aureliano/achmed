696b9a1bb7a58fb3061c457d269bbb12d1f198d8
aureliano <aureliano.franca@hotmail.com>
2015-12-09 18:19:22 -0200

    Implement start process.

--- src/main/java/com/github/aureliano/achmed/os/service/DebianService.java
+++ src/main/java/com/github/aureliano/achmed/os/service/DebianService.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 
 import com.github.aureliano.achmed.command.CommandFacade;
 import com.github.aureliano.achmed.command.CommandResponse;
+import com.github.aureliano.achmed.helper.StringHelper;
 
 public class DebianService extends LinuxService {
 
@@ -12,6 +13,20 @@ public class DebianService extends LinuxService {
 	
 	public DebianService() {
 		super();
+	}
+	
+	@Override
+	public CommandResponse start() {
+		if (this.isRunning()) {
+			logger.debug("Service " + super.properties.getName() + " is already running.");
+			return null;
+		}
+		
+		if (StringHelper.isEmpty(super.properties.getBinary())) {
+			return CommandFacade.executeCommand(SERVICE_APP, super.properties.getName(), "start");
+		}
+		
+		return super.start();
 	}
 	
 	@Override
