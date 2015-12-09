391b24f35e79d3dd287f8348043f60d86701d60c
aureliano <aureliano.franca@hotmail.com>
2015-12-09 16:06:25 -0200

    Remove unimplemented methods.

--- src/main/java/com/github/aureliano/achmed/os/service/BaseService.java
+++ src/main/java/com/github/aureliano/achmed/os/service/BaseService.java
@@ -1,6 +1,5 @@
 package com.github.aureliano.achmed.os.service;
 
-import com.github.aureliano.achmed.command.CommandResponse;
 import com.github.aureliano.achmed.resources.properties.ServiceProperties;
 
 public abstract class BaseService implements IService {
@@ -9,22 +8,6 @@ public abstract class BaseService implements IService {
 	
 	public BaseService() {
 		super();
-	}
-
-	public CommandResponse start() {
-		throw new UnsupportedOperationException("Not implemented yet.");
-	}
-
-	public CommandResponse stop() {
-		throw new UnsupportedOperationException("Not implemented yet.");
-	}
-
-	public CommandResponse restart() {
-		throw new UnsupportedOperationException("Not implemented yet.");
-	}
-
-	public boolean isRunning() {
-		throw new UnsupportedOperationException("Not implemented yet.");
 	}
 
 	public void setServiceProperties(ServiceProperties properties) {
