8a8b00b59e3b4a1cc9a1c88d0f6f90d3d8b150f5
aureliano <aureliano.franca@hotmail.com>
2015-12-10 11:02:12 -0200

    Implement apply method of service resource.

--- src/main/java/com/github/aureliano/achmed/resources/ServiceResource.java
+++ src/main/java/com/github/aureliano/achmed/resources/ServiceResource.java
@@ -1,10 +1,16 @@
 package com.github.aureliano.achmed.resources;
 
+import org.apache.log4j.Logger;
+
+import com.github.aureliano.achmed.AppConfiguration;
+import com.github.aureliano.achmed.os.service.IService;
 import com.github.aureliano.achmed.resources.properties.IResourceProperties;
 import com.github.aureliano.achmed.resources.properties.ServiceProperties;
 
 public class ServiceResource implements IResource {
 
+	private static final Logger logger = Logger.getLogger(ServiceResource.class);
+	
 	private ServiceProperties properties;
 	
 	public ServiceResource() {
@@ -16,6 +22,22 @@ public class ServiceResource implements IResource {
 	}
 	
 	public void apply() {
+		logger.info(" >>> Apply service resource " + this.properties.getName());
+		
+		this.properties.configureAttributes();
+		 IService service = AppConfiguration.instance().getOperatingSystem().getDefaultServiceManager();
+		 service.setServiceProperties(this.properties);
+		
+		 if (this.properties.isEnsure()) {
+			if (!service.isRunning()) {
+				service.start();
+			}
+		} else {
+			if (service.isRunning()) {
+				service.stop();
+			}
+		}
+		
 		throw new UnsupportedOperationException("Not implemented yet.");
 	}
 	
