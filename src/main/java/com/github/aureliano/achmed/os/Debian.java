a937f71b4954e7dffe058ad257d97aa06e05d0cb
aureliano <aureliano.franca@hotmail.com>
2015-12-10 10:46:35 -0200

    Add method to retrieve default service manager.

--- src/main/java/com/github/aureliano/achmed/os/Debian.java
+++ src/main/java/com/github/aureliano/achmed/os/Debian.java
@@ -2,19 +2,28 @@ package com.github.aureliano.achmed.os;
 
 import com.github.aureliano.achmed.os.pkg.IPackageManager;
 import com.github.aureliano.achmed.os.pkg.PackageManagerFactory;
+import com.github.aureliano.achmed.os.service.DebianService;
+import com.github.aureliano.achmed.os.service.IService;
 import com.github.aureliano.achmed.types.OperatingSystemFamily;
 import com.github.aureliano.achmed.types.PackageProvider;
 
 public class Debian extends Linux {
 
 	private IPackageManager packageManager;
+	private IService serviceManager;
 	
 	public Debian() {
 		this.packageManager = PackageManagerFactory.createPackageManager(PackageProvider.APT);
+		this.serviceManager = new DebianService();
 	}
 
 	public IPackageManager getDefaultPackageManager() {
 		return this.packageManager;
+	}
+	
+	@Override
+	public IService getDefaultServiceManager() {
+		return this.serviceManager;
 	}
 
 	public OperatingSystemFamily getOperatingSystemFamily() {
--- src/main/java/com/github/aureliano/achmed/os/IOperatingSystem.java
+++ src/main/java/com/github/aureliano/achmed/os/IOperatingSystem.java
@@ -2,12 +2,15 @@ package com.github.aureliano.achmed.os;
 
 import com.github.aureliano.achmed.command.CommandResponse;
 import com.github.aureliano.achmed.os.pkg.IPackageManager;
+import com.github.aureliano.achmed.os.service.IService;
 import com.github.aureliano.achmed.types.OS;
 import com.github.aureliano.achmed.types.OperatingSystemFamily;
 
 public interface IOperatingSystem {
 
 	public abstract IPackageManager getDefaultPackageManager();
+	
+	public abstract IService getDefaultServiceManager();
 	
 	public abstract OS getOperatingSystem();
 	
