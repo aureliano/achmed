51199d779956daf31660430f06d425762985753a
aureliano <aureliano.franca@hotmail.com>
2015-12-10 10:49:40 -0200

    Make Ubuntu extend Debian.

--- src/main/java/com/github/aureliano/achmed/os/Ubuntu.java
+++ src/main/java/com/github/aureliano/achmed/os/Ubuntu.java
@@ -1,20 +1,11 @@
 package com.github.aureliano.achmed.os;
 
-import com.github.aureliano.achmed.os.pkg.IPackageManager;
-import com.github.aureliano.achmed.os.pkg.PackageManagerFactory;
 import com.github.aureliano.achmed.types.OperatingSystemFamily;
-import com.github.aureliano.achmed.types.PackageProvider;
 
-public class Ubuntu extends Linux {
-	
-	private IPackageManager packageManager;
+public class Ubuntu extends Debian {
 	
 	public Ubuntu() {
-		this.packageManager = PackageManagerFactory.createPackageManager(PackageProvider.APT);
-	}
-
-	public IPackageManager getDefaultPackageManager() {
-		return this.packageManager;
+		super();
 	}
 
 	public OperatingSystemFamily getOperatingSystemFamily() {
