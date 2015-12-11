733576f402b3c3306a3fbb31a014b21c8dafba38
aureliano <aureliano.franca@hotmail.com>
2015-12-11 11:00:54 -0200

    Implement method to check whether service is running.

--- src/main/java/com/github/aureliano/achmed/os/service/RedHatService.java
+++ src/main/java/com/github/aureliano/achmed/os/service/RedHatService.java
@@ -1,11 +1,27 @@
 package com.github.aureliano.achmed.os.service;
 
+import org.apache.log4j.Logger;
+
+import com.github.aureliano.achmed.command.CommandFacade;
 import com.github.aureliano.achmed.command.CommandResponse;
 
 public class RedHatService extends LinuxService {
 
+	private static final Logger logger = Logger.getLogger(RedHatService.class);
+	private static final String SERVICE = "/sbin/service";
+	
 	public RedHatService() {
 		super();
+	}
+	
+	@Override
+	public boolean isRunning() {
+		if ((super.properties.getHasStatus() != null) && (super.properties.getHasStatus())) {
+			CommandResponse res = CommandFacade.executeCommand(SERVICE, super.properties.getName(), "status");
+			return (res.getExitStatusCode() == 0);
+		}
+		
+		return super.isRunning();
 	}
 
 	@Override
