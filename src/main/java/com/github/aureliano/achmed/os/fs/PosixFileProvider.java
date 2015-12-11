3619c11e23ab8201877c148f044afff4c1899366
aureliano <aureliano.franca@hotmail.com>
2015-12-11 18:01:44 -0200

    Implement set file owner.

--- src/main/java/com/github/aureliano/achmed/os/fs/PosixFileProvider.java
+++ src/main/java/com/github/aureliano/achmed/os/fs/PosixFileProvider.java
@@ -1,12 +1,20 @@
 package com.github.aureliano.achmed.os.fs;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
 import com.github.aureliano.achmed.command.CommandFacade;
 import com.github.aureliano.achmed.command.CommandResponse;
 import com.github.aureliano.achmed.exception.FileResourceException;
+import com.github.aureliano.achmed.helper.StringHelper;
 import com.github.aureliano.achmed.resources.properties.FileProperties;
 
 public class PosixFileProvider implements IFileProvider {
 
+	private static final Logger logger = Logger.getLogger(PosixFileProvider.class);
+	
 	private FileProperties properties;
 	
 	public PosixFileProvider() {
@@ -25,7 +33,30 @@ public class PosixFileProvider implements IFileProvider {
 
 	@Override
 	public void setFileOwner() {
-		throw new UnsupportedOperationException("Not implemented yet.");
+		List<String> ownerAndGroup = new ArrayList<String>();
+		if (!StringHelper.isEmpty(this.properties.getOwner())) {
+			ownerAndGroup.add(this.properties.getOwner());
+		}
+		
+		if ((!StringHelper.isEmpty(this.properties.getOwner())) && (!StringHelper.isEmpty(this.properties.getGroup()))) {
+			ownerAndGroup.add(":");
+		}
+		
+		if (!StringHelper.isEmpty(this.properties.getGroup())) {
+			ownerAndGroup.add(this.properties.getOwner());
+		}
+		
+		if (ownerAndGroup.isEmpty()) {
+			logger.warn("Ignore setting file owner because no owner neither group was provided.");
+			return;
+		}
+		
+		ownerAndGroup.add(0, "chown");
+		CommandResponse res = CommandFacade.executeCommand(ownerAndGroup.toArray(new String[0]));
+		
+		if (!res.isOK()) {
+			throw new FileResourceException(res);
+		}
 	}
 
 	@Override
