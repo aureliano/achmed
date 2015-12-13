7b5425358b2acd6824d2f2626f310f23e8ab3afe
aureliano <aureliano.franca@hotmail.com>
2015-12-13 17:44:21 -0200

    Create file helper.

---
+++ src/main/java/com/github/aureliano/achmed/helper/FileHelper.java
@@ -0,0 +1,9 @@
+package com.github.aureliano.achmed.helper;
+
+
+public final class FileHelper {
+
+	private FileHelper() {
+		throw new InstantiationError(this.getClass().getName() + " cannot be instantiated.");
+	}
+}
