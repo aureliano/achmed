9744357c0a894f305c4bdba71ae1897742b3c3a4
aureliano <aureliano.franca@hotmail.com>
2015-12-15 13:43:47 -0200

    Create Size annotation.

---
+++ src/main/java/com/github/aureliano/achmed/annotation/Size.java
@@ -0,0 +1,20 @@
+package com.github.aureliano.achmed.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.github.aureliano.achmed.validation.SizeValidator;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Constraint(validatedBy = SizeValidator.class)
+public @interface Size {
+
+	public abstract String message() default "Expected field #{0} to have size between #{1} and #{2} but got #{3}.";
+	
+	public abstract int min();
+	
+	public abstract int max();
+}
