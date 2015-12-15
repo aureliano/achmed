c27820b77d7f63a74523d082b9dbe5d3d8a74725
aureliano <aureliano.franca@hotmail.com>
2015-12-15 13:44:39 -0200

    Create size validator.

---
+++ src/main/java/com/github/aureliano/achmed/validation/SizeValidator.java
@@ -0,0 +1,56 @@
+package com.github.aureliano.achmed.validation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.github.aureliano.achmed.annotation.Size;
+import com.github.aureliano.achmed.helper.ReflectionHelper;
+import com.github.aureliano.achmed.helper.StringHelper;
+
+public class SizeValidator implements IValidator {
+
+	public SizeValidator() {
+		super();
+	}
+	
+	@Override
+	public Set<ConstraintViolation> validate(Object object, Method method, Annotation annotation) {
+		String property = ReflectionHelper.getSimpleAccessMethodName(method);
+		Object returnedValue = ReflectionHelper.callMethod(object, method.getName(), null, null);
+		
+		if (returnedValue == null) {
+			return null;
+		}
+		
+		Set<ConstraintViolation> violations = new HashSet<ConstraintViolation>();
+		Size sizeAnnotation = (Size) annotation;
+		String message = sizeAnnotation.message();
+		int minSize = sizeAnnotation.min();
+		int maxSize = sizeAnnotation.max();
+		int objectSize;
+		
+		if (Collection.class.isAssignableFrom(returnedValue.getClass())) {
+			objectSize = ((Collection<?>) returnedValue).size();
+		} else if (!(returnedValue instanceof String) && StringHelper.isNumeric(returnedValue.toString())) {
+			Double d = Double.parseDouble(returnedValue.toString());
+			objectSize = d.intValue();
+		} else {
+			objectSize = returnedValue.toString().length();
+		}
+		
+		if ((minSize > objectSize) || (maxSize < objectSize)) {
+			violations.add(new ConstraintViolation()
+				.withValidator(Size.class)
+				.withMessage(message
+					.replaceFirst("#\\{0\\}", property)
+					.replaceFirst("#\\{1\\}", String.valueOf(minSize))
+					.replaceFirst("#\\{2\\}", String.valueOf(maxSize))
+					.replaceFirst("#\\{3\\}", String.valueOf(objectSize))));
+		}
+		
+		return violations;
+	}
+}
---
+++ src/test/java/com/github/aureliano/achmed/validation/SizeValidatorTest.java
@@ -0,0 +1,92 @@
+package com.github.aureliano.achmed.validation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.aureliano.achmed.Model;
+import com.github.aureliano.achmed.annotation.Size;
+
+public class SizeValidatorTest {
+
+	private SizeValidator validator;
+	
+	public SizeValidatorTest() {
+		this.validator = new SizeValidator();
+	}
+	
+	@Test
+	public void testValidateWithStringMinError() throws SecurityException, NoSuchMethodException {
+		Model model = new Model().withId("no");
+		Method method = model.getClass().getMethod("getId", new Class[] {});
+		Annotation annotation = method.getAnnotation(Size.class);
+		 
+		ConstraintViolation constraint = this.validator.validate(model, method, annotation).iterator().next();
+		Assert.assertNotNull(constraint);
+		
+		Assert.assertEquals("Expected field id to have size between 3 and 5 but got 2.", constraint.getMessage());
+		Assert.assertEquals(Size.class, constraint.getValidator());
+	}
+	
+	@Test
+	public void testValidateWithCollectionMinError() throws SecurityException, NoSuchMethodException {
+		Model model = new Model();
+		Method method = model.getClass().getMethod("getExceptions", new Class[] {});
+		Annotation annotation = method.getAnnotation(Size.class);
+		 
+		ConstraintViolation constraint = this.validator.validate(model, method, annotation).iterator().next();
+		Assert.assertNotNull(constraint);
+		
+		Assert.assertEquals("Expected field exceptions to have size between 1 and 1 but got 0.", constraint.getMessage());
+		Assert.assertEquals(Size.class, constraint.getValidator());
+	}
+	
+	@Test
+	public void testValidateWithStringMaxError() throws SecurityException, NoSuchMethodException {
+		Model model = new Model().withId("Is it ok?");
+		Method method = model.getClass().getMethod("getId", new Class[] {});
+		Annotation annotation = method.getAnnotation(Size.class);
+		 
+		ConstraintViolation constraint = this.validator.validate(model, method, annotation).iterator().next();
+		Assert.assertNotNull(constraint);
+		
+		Assert.assertEquals("Expected field id to have size between 3 and 5 but got 9.", constraint.getMessage());
+		Assert.assertEquals(Size.class, constraint.getValidator());
+	}
+	
+	@Test
+	public void testValidateWithCollectionMaxError() throws SecurityException, NoSuchMethodException {
+		Model model = new Model()
+			.addException("1")
+			.addException("2");
+		Method method = model.getClass().getMethod("getExceptions", new Class[] {});
+		Annotation annotation = method.getAnnotation(Size.class);
+		 
+		ConstraintViolation constraint = this.validator.validate(model, method, annotation).iterator().next();
+		Assert.assertNotNull(constraint);
+		
+		Assert.assertEquals("Expected field exceptions to have size between 1 and 1 but got 2.", constraint.getMessage());
+		Assert.assertEquals(Size.class, constraint.getValidator());
+	}
+	
+	@Test
+	public void testValidate() throws SecurityException, NoSuchMethodException {
+		Model model = new Model()
+			.withId("yes!")
+			.addException("1");
+		Method method = model.getClass().getMethod("getId", new Class[] {});
+		Annotation annotation = method.getAnnotation(Size.class);
+		 
+		Set<ConstraintViolation> res = this.validator.validate(model, method, annotation);
+		Assert.assertTrue(res.isEmpty());
+		
+		method = model.getClass().getMethod("getExceptions", new Class[] {});
+		annotation = method.getAnnotation(Size.class);
+		
+		res = this.validator.validate(model, method, annotation);
+		Assert.assertTrue(res.isEmpty());
+	}
+}
