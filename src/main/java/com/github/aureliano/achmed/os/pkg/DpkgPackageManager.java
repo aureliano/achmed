6c348722943298a011e84b354a6865e1d866777e
aureliano <aureliano.franca@hotmail.com>
2015-12-15 11:04:49 -0200

    Implement methodo to check wheter package is installed.

--- src/main/java/com/github/aureliano/achmed/os/pkg/DpkgPackageManager.java
+++ src/main/java/com/github/aureliano/achmed/os/pkg/DpkgPackageManager.java
@@ -1,9 +1,15 @@
 package com.github.aureliano.achmed.os.pkg;
 
+import com.github.aureliano.achmed.command.CommandFacade;
 import com.github.aureliano.achmed.command.CommandResponse;
+import com.github.aureliano.achmed.exception.PackageResourceException;
+import com.github.aureliano.achmed.helper.StringHelper;
 import com.github.aureliano.achmed.resources.properties.PackageProperties;
 
 public class DpkgPackageManager implements IPackageManager {
+	
+	private static final String DPKG_QUERY = "/usr/bin/dpkg-query";
+	private static final String QUERY_FORMAT = "'${status} ${version}\\n'";
 
 	private PackageProperties properties;
 	
@@ -21,6 +27,24 @@ public class DpkgPackageManager implements IPackageManager {
 
 	public String latest() {
 		throw new UnsupportedOperationException("Method not implemented yet.");
+	}
+	
+	public boolean isInstalled() {
+		final String desiredStatus = "installed";
+		CommandResponse res = CommandFacade.executeCommand(
+			DPKG_QUERY, "-W", "-f", QUERY_FORMAT, this.properties.getName()
+		);
+		
+		if (!res.isOK()) {
+			throw new PackageResourceException(res);
+		}
+		
+		String[] match = StringHelper.match("^(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S*)$", res.getOutput());
+		if (match == null) {
+			throw new PackageResourceException("Failed to match dpkg-query " + res.getCommand());
+		}
+		
+		return desiredStatus.equalsIgnoreCase(match[3]);
 	}
 
 	public void setPackageProperties(PackageProperties properties) {
