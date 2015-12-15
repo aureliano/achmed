68c7c0dad5fa75bad57c4065b0bc0bbe4a3b1235
aureliano <aureliano.franca@hotmail.com>
2015-12-15 12:33:27 -0200

    Test constraint violation.

--- src/test/java/com/github/aureliano/achmed/validation/ConstraintViolationTest.java
+++ src/test/java/com/github/aureliano/achmed/validation/ConstraintViolationTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import com.sun.istack.internal.NotNull;
+import com.github.aureliano.achmed.annotation.NotNull;
 
 public class ConstraintViolationTest {
 
