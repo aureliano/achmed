6a534754d145fef91388003a9fa005887a9a8eaf
aureliano <aureliano.franca@hotmail.com>
2015-12-15 13:11:43 -0200

    Create NotEmpty annotation.

---
+++ src/main/java/com/github/aureliano/achmed/annotation/NotEmpty.java
@@ -0,0 +1,16 @@
+package com.github.aureliano.achmed.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.github.aureliano.achmed.validation.NotEmptyValidator;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Constraint(validatedBy = NotEmptyValidator.class)
+public @interface NotEmpty {
+
+	public abstract String message() default "Expected to find a not empty text for field #{0}.";
+}
