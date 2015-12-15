3ad184efa37812ae9c2fc9f461dbf10a24c62a17
aureliano <aureliano.franca@hotmail.com>
2015-12-15 12:33:46 -0200

    Add method to call method through reflection.

--- src/main/java/com/github/aureliano/achmed/helper/ReflectionHelper.java
+++ src/main/java/com/github/aureliano/achmed/helper/ReflectionHelper.java
@@ -2,11 +2,23 @@ package com.github.aureliano.achmed.helper;
 
 import java.lang.reflect.Method;
 
+import com.github.aureliano.achmed.exception.AchmedException;
+
 public final class ReflectionHelper {
 
 	private ReflectionHelper() {
 		throw new InstantiationError(this.getClass().getName() + " cannot be instantiated.");
 	}
+    
+    public static Object callMethod(Object object, String methodName, Class<?>[] parametersType, Object[] methodParameters) {
+    	Class<?> clazz = object.getClass();
+    	try {
+    		Method method = clazz.getMethod(methodName, parametersType);
+    		return method.invoke(object, methodParameters);
+    	} catch (Exception ex) {
+    		throw new AchmedException(ex);
+    	}
+    }
     
     public static String getSimpleAccessMethodName(Method method) {
     	String name = method.getName().replaceFirst("^(get|is|set|with)", "");
