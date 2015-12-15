e2240b4cb03651e9501d129a06ebaaaf76a456d5
aureliano <aureliano.franca@hotmail.com>
2015-12-15 16:31:35 -0200

    Implement apply resources method.

--- src/main/java/com/github/aureliano/achmed/Agent.java
+++ src/main/java/com/github/aureliano/achmed/Agent.java
@@ -2,10 +2,18 @@ package com.github.aureliano.achmed;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.log4j.Logger;
+
+import com.github.aureliano.achmed.exception.AchmedValidationException;
 import com.github.aureliano.achmed.resources.IResource;
+import com.github.aureliano.achmed.validation.ConstraintViolation;
+import com.github.aureliano.achmed.validation.ObjectValidator;
 
 public class Agent {
+	
+	private static final Logger logger = Logger.getLogger(Agent.class);
 
 	private List<IResource> resources;
 	
@@ -28,6 +36,37 @@ public class Agent {
 	}
 	
 	public void apply() {
-		throw new UnsupportedOperationException("Not implemented exception.");
+		logger.info("Preparing to apply " + this.resources.size() + " resources.");
+		logger.info("Applying resource validations.");
+		
+		this.validate();
+		
+		logger.info("Resources validated.");
+		logger.info("Applying resources.");
+		
+		for (IResource resource : this.resources) {
+			resource.apply();
+		}
+	}
+	
+	private void validate() {
+		boolean failed = false;
+		ObjectValidator validator = ObjectValidator.instance();
+		
+		for (IResource resource : this.resources) {
+			Set<ConstraintViolation> violations = validator.validate(resource);
+			if (violations.isEmpty()) {
+				continue;
+			}
+			
+			for (ConstraintViolation violation : violations) {
+				failed = true;
+				logger.error(violation.getMessage());
+			}
+		}
+		
+		if (failed) {
+			throw new AchmedValidationException("Resource validation has failed.");
+		}
 	}
 }
