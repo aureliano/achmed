b51c85121b3ef9aa6ffd9542b92653623900af12
aureliano <aureliano.franca@hotmail.com>
2015-12-15 13:12:02 -0200

    Create not empty validator.

---
+++ src/main/java/com/github/aureliano/achmed/validation/NotEmptyValidator.java
@@ -0,0 +1,39 @@
+package com.github.aureliano.achmed.validation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.github.aureliano.achmed.annotation.NotEmpty;
+import com.github.aureliano.achmed.exception.AchmedException;
+import com.github.aureliano.achmed.helper.ReflectionHelper;
+import com.github.aureliano.achmed.helper.StringHelper;
+
+public class NotEmptyValidator implements IValidator {
+
+	public NotEmptyValidator() {
+		super();
+	}
+
+	@Override
+	public Set<ConstraintViolation> validate(Object object, Method method, Annotation annotation) {
+		String property = ReflectionHelper.getSimpleAccessMethodName(method);
+		Object returnedValue = ReflectionHelper.callMethod(object, method.getName(), null, null);
+		Set<ConstraintViolation> violations = new HashSet<ConstraintViolation>();
+		
+		if ((returnedValue != null) && !(returnedValue instanceof String)) {
+			throw new AchmedException("Expected to find a String but got " + returnedValue.getClass().getName() +
+					". You might want to use NotNull annotation instead of NotEmpty.");
+		}
+		
+		if (StringHelper.isEmpty(StringHelper.parse(returnedValue))) {
+			String message = ((NotEmpty) annotation).message();
+			violations.add(new ConstraintViolation()
+				.withValidator(NotEmpty.class)
+				.withMessage(message.replaceFirst("#\\{0\\}", property)));
+		}
+		
+		return violations;
+	}
+}
---
+++ src/test/java/com/github/aureliano/achmed/validation/NotEmptyValidatorTest.java
@@ -0,0 +1,56 @@
+package com.github.aureliano.achmed.validation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.aureliano.achmed.Model;
+import com.github.aureliano.achmed.annotation.NotEmpty;
+
+public class NotEmptyValidatorTest {
+
+	private NotEmptyValidator validator;
+	
+	public NotEmptyValidatorTest() {
+		this.validator = new NotEmptyValidator();
+	}
+	
+	@Test
+	public void testValidateNullWithError() throws SecurityException, NoSuchMethodException {
+		Model configuration = new Model().withId(null);
+		Method method = configuration.getClass().getMethod("getId", new Class[] {});
+		Annotation annotation = method.getAnnotation(NotEmpty.class);
+		 
+		ConstraintViolation constraint = this.validator.validate(configuration, method, annotation).iterator().next();
+		Assert.assertNotNull(constraint);
+		
+		Assert.assertEquals("Expected to find a not empty text for field id.", constraint.getMessage());
+		Assert.assertEquals(NotEmpty.class, constraint.getValidator());
+	}
+	
+	@Test
+	public void testValidateEmptyWithError() throws SecurityException, NoSuchMethodException {
+		Model configuration = new Model().withId("");
+		Method method = configuration.getClass().getMethod("getId", new Class[] {});
+		Annotation annotation = method.getAnnotation(NotEmpty.class);
+		 
+		ConstraintViolation constraint = this.validator.validate(configuration, method, annotation).iterator().next();
+		Assert.assertNotNull(constraint);
+		
+		Assert.assertEquals("Expected to find a not empty text for field id.", constraint.getMessage());
+		Assert.assertEquals(NotEmpty.class, constraint.getValidator());
+	}
+	
+	@Test
+	public void testValidate() throws SecurityException, NoSuchMethodException {
+		Model configuration = new Model().withId("custom-id");
+		Method method = configuration.getClass().getMethod("getId", new Class[] {});
+		Annotation annotation = method.getAnnotation(NotEmpty.class);
+		
+		Set<ConstraintViolation> res = this.validator.validate(configuration, method, annotation);
+		Assert.assertTrue(res.isEmpty());
+	}
+}
