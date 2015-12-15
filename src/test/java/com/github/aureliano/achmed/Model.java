a5aa376eba389917fd06e70edaa6dfd1d81f79df
aureliano <aureliano.franca@hotmail.com>
2015-12-15 13:31:57 -0200

    Add min annotation.

--- src/test/java/com/github/aureliano/achmed/Model.java
+++ src/test/java/com/github/aureliano/achmed/Model.java
@@ -1,5 +1,9 @@
 package com.github.aureliano.achmed;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.aureliano.achmed.annotation.Min;
 import com.github.aureliano.achmed.annotation.NotEmpty;
 import com.github.aureliano.achmed.annotation.NotNull;
 import com.github.aureliano.achmed.annotation.Pattern;
@@ -7,14 +11,16 @@ import com.github.aureliano.achmed.annotation.Pattern;
 public class Model {
 
 	private String id;
+	private List<String> exceptions;
 	
 	public Model() {
-		super();
+		this.exceptions = new ArrayList<>();
 	}
 
 	@NotNull
 	@NotEmpty
 	@Pattern(value = "[\\d\\w]{3,5}")
+	@Min(value = 3)
 	public String getId() {
 		return this.id;
 	}
@@ -22,5 +28,15 @@ public class Model {
 	public Model withId(String id) {
 		this.id = id;
 		return this;
+	}
+
+	public Model addException(String ex) {
+		this.exceptions.add(ex);
+		return this;
+	}
+
+	@Min(value = 1)
+	public List<String> getExceptions() {
+		return this.exceptions;
 	}
 }
