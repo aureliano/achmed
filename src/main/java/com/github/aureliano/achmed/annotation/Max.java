ce01cb702346e44337229c152c7053ddc8ae2f9c
aureliano <aureliano.franca@hotmail.com>
2015-12-15 13:37:25 -0200

    Create max annotation.

---
+++ src/main/java/com/github/aureliano/achmed/annotation/Max.java
@@ -0,0 +1,18 @@
+package com.github.aureliano.achmed.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.github.aureliano.achmed.validation.MaxValidator;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Constraint(validatedBy = MaxValidator.class)
+public @interface Max {
+
+	public abstract String message() default "Expected a maximum value of #{0} for field #{1} but got #{2}.";
+	
+	public abstract int value();
+}
