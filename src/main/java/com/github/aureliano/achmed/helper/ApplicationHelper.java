ab3a3ed411c6913b6f5bec78a0d9cc680e95d67a
aureliano <aureliano.franca@hotmail.com>
2015-12-16 09:59:14 -0200

    Add method to print version.

--- src/main/java/com/github/aureliano/achmed/helper/ApplicationHelper.java
+++ src/main/java/com/github/aureliano/achmed/helper/ApplicationHelper.java
@@ -1,13 +1,28 @@
 package com.github.aureliano.achmed.helper;
 
+import java.util.Properties;
+
 public final class ApplicationHelper {
 
 	private ApplicationHelper() {
 		throw new InstantiationError(this.getClass().getName() + " cannot be instantiated.");
 	}
 	
-	public static void printHelp() {
+	public static String help() {
 		String help = FileHelper.readResource("meta/help");
-		System.out.println(help);
+		return help;
+	}
+	
+	public static String version() {
+		Properties properties = PropertyHelper.loadProperties("meta/version.properties");
+		return new StringBuilder("achmed")
+			.append(" ")
+			.append(properties.get("version"))
+			.append(":")
+			.append(properties.get("release"))
+			.append(" (")
+			.append(properties.get("date"))
+			.append(")")
+			.toString();
 	}
 }
