b2ab5dac7c10931abf9fa18d2ad8d79d77bb72c4
aureliano <aureliano.franca@hotmail.com>
2015-12-16 14:38:33 -0200

    Configure status to return when application shuts down.

--- src/main/java/com/github/aureliano/achmed/Application.java
+++ src/main/java/com/github/aureliano/achmed/Application.java
@@ -1,6 +1,5 @@
 package com.github.aureliano.achmed;
 
-import com.github.aureliano.achmed.exception.AchmedException;
 import com.github.aureliano.achmed.helper.ApplicationHelper;
 import com.github.aureliano.achmed.helper.StringHelper;
 
@@ -20,48 +19,60 @@ public class Application {
 			return;
 		}
 		
+		int status = 0;
 		if ((args[0].equals("-h")) || (args[0].equals("--help")) || (args[0].equals("help"))) {
-			this.printHelp();
+			status = this.printHelp();
 		} else if ((args[0].equals("-v")) || (args[0].equals("--version")) || (args[0].equals("version"))) {
-			this.printVersion();
+			status = this.printVersion();
 		} else if (args[0].equals("burst")) {
-			this.handleExecution(args);
+			status = this.handleExecution(args);
 		} else {
-			this.printError(args);
-			System.exit(1);
+			status = this.printError(args);
 		}
 		
-		System.exit(0);
+		System.exit(status);
 	}
 	
-	protected void handleExecution(String[] args) {
+	protected int handleExecution(String[] args) {
 		if (args.length == 1) {
-			this.prepareExecution(null);
+			return this.prepareExecution(null);
 		} else if (args.length != 2) {
-			throw new AchmedException("Invalid arguments. You can pass just one file path after burst command.");
+			System.err.println("Invalid arguments. You can pass just one file path after burst command.");
+			return 1;
 		} else {
-			this.prepareExecution(args[1]);
+			return this.prepareExecution(args[1]);
 		}
 	}
 
-	protected void prepareExecution(String path) {
+	protected int prepareExecution(String path) {
 		if (StringHelper.isEmpty(path)) {
 			System.out.println(ApplicationHelper.error(new String[0]));
-			System.exit(1);
+			return 1;
 		}
 		
-		ApplicationHelper.execute(path);
+		try {
+			ApplicationHelper.execute(path);
+			return 0;
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+			ex.printStackTrace();
+			
+			return 2;
+		}
 	}
 
-	protected void printHelp() {
+	protected int printHelp() {
 		System.out.println(ApplicationHelper.help());
+		return 0;
 	}
 	
-	protected void printVersion() {
+	protected int printVersion() {
 		System.out.println(ApplicationHelper.version());
+		return 0;
 	}
 	
-	private void printError(String[] args) {
+	private int printError(String[] args) {
 		System.out.println(ApplicationHelper.error(args));
+		return 1;
 	}
 }
