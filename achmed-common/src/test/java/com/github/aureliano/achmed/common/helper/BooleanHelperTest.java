549b0a4ade11deca4561a24f587fcb84268d8f4e
aureliano <aureliano.franca@hotmail.com>
2016-04-21 18:06:55 -0300

    Move boolean helper to common project.

--- achmed-common/src/test/java/com/github/aureliano/achmed/common/helper/BooleanHelperTest.java
+++ achmed-common/src/test/java/com/github/aureliano/achmed/common/helper/BooleanHelperTest.java
@@ -1,10 +1,12 @@
-package com.github.aureliano.achmed.helper;
+package com.github.aureliano.achmed.common.helper;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import com.github.aureliano.achmed.common.helper.BooleanHelper;
 
 public class BooleanHelperTest {
 
