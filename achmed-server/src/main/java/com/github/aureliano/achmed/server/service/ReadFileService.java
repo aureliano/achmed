35ad39aa248a94f98a621f923c8828b7ae7bcb7c
aureliano <aureliano.franca@hotmail.com>
2016-04-22 14:00:12 -0300

    Create read file service.

---
+++ achmed-server/src/main/java/com/github/aureliano/achmed/server/service/ReadFileService.java
@@ -0,0 +1,13 @@
+package com.github.aureliano.achmed.server.service;
+
+import java.net.Socket;
+
+import com.github.aureliano.achmed.common.exception.AchmedException;
+
+public class ReadFileService implements IService {
+
+	@Override
+	public void consume(Socket socket) {
+		throw new AchmedException("Not implemented yet.");
+	}
+}
