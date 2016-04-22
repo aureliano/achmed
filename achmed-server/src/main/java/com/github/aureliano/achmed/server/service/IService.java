5a0870d0ae21c799fc4d2666bf74888d78a16b3b
aureliano <aureliano.franca@hotmail.com>
2016-04-22 19:11:44 -0300

    Add output stream parameter.

--- achmed-server/src/main/java/com/github/aureliano/achmed/server/service/IService.java
+++ achmed-server/src/main/java/com/github/aureliano/achmed/server/service/IService.java
@@ -1,8 +1,9 @@
 package com.github.aureliano.achmed.server.service;
 
+import java.io.OutputStream;
 import java.util.Map;
 
 public interface IService {
 
-	public abstract byte[] consume(Map<String, String> parameters);
+	public abstract byte[] consume(OutputStream outputStream, Map<String, String> parameters);
 }
