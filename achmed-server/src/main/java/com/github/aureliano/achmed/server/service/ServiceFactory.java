c6c7875eddd136552882262637baafe5b5d6aa95
aureliano <aureliano.franca@hotmail.com>
2016-04-24 11:21:34 -0300

    Add method to create service by request string.

--- achmed-server/src/main/java/com/github/aureliano/achmed/server/service/ServiceFactory.java
+++ achmed-server/src/main/java/com/github/aureliano/achmed/server/service/ServiceFactory.java
@@ -4,6 +4,9 @@ import com.github.aureliano.achmed.common.exception.AchmedException;
 
 public final class ServiceFactory {
 
+	private static final String SERVICE_PATTERN = "^service:\\s*";
+	private static final String SERVICE_NAME_PATTERN = SERVICE_PATTERN + "(read_file)$";
+	
 	private ServiceFactory() {}
 	
 	public static IService createService(ServiceType type) {
@@ -13,5 +16,28 @@ public final class ServiceFactory {
 		default:
 			throw new AchmedException("Unsupported service type: " + type);
 		}
+	}
+	
+	public static IService createService(String data) {
+		ServiceType type = getServiceType(data);
+		return ServiceFactory.createService(type);
+	}
+	
+	private static ServiceType getServiceType(String data) {
+		String serviceName = getRequestedService(data);
+		try {
+			return ServiceType.valueOf(serviceName);
+		} catch (Exception ex) {
+			throw new AchmedException("There is not a service named " + serviceName);
+		}
+	}
+	
+	private static String getRequestedService(String data) {
+		if (!data.matches(SERVICE_NAME_PATTERN)) {
+			throw new AchmedException("Invalid service: " + data);
+		}
+		
+		String serviceName = data.split(":")[1].trim();
+		return serviceName.toUpperCase();
 	}
 }
--- achmed-server/src/test/java/com/github/aureliano/achmed/server/service/ServiceFactoryTest.java
+++ achmed-server/src/test/java/com/github/aureliano/achmed/server/service/ServiceFactoryTest.java
@@ -4,11 +4,29 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.github.aureliano.achmed.common.exception.AchmedException;
+
 public class ServiceFactoryTest {
 
 	@Test
-	public void testCreateService() {
+	public void testCreateServiceByType() {
 		IService service = ServiceFactory.createService(ServiceType.READ_FILE);
+		assertTrue(service instanceof CheckFileStatusService);
+	}
+	
+	@Test(expected = AchmedException.class)
+	public void testCreateServiceByRequestStringError1() {
+		ServiceFactory.createService("");
+	}
+	
+	@Test(expected = AchmedException.class)
+	public void testCreateServiceByRequestStringError2() {
+		ServiceFactory.createService("invalid");
+	}
+	
+	@Test
+	public void testCreateServiceByRequestString() {
+		IService service = ServiceFactory.createService("service: read_file");
 		assertTrue(service instanceof CheckFileStatusService);
 	}
 }
